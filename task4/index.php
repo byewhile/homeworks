443684dd1a49bc23b99f2017348c970d63764b02
Aleksey <93428940+byewhile@users.noreply.github.com>
2024-09-24 17:45:11 +0300

    fix

--- task4/index.php
+++ task4/index.php
@@ -28,6 +28,16 @@ $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
         $sql = "INSERT INTO `first_work` (value) VALUES ('$fullName')";
         $conn->query($sql);
     }
+
+    if (isset($_POST["fullNameEdit"]) || isset($_POST["date"])) {
+        $id = $_POST["id"];
+        $fullName = trim($_POST["fullNameEdit"]);
+        $date = $_POST["date"];
+    
+        $sql = "UPDATE `first_work` SET value = '$fullName', action_date = '$date' WHERE id = '$id'";
+        $conn->query($sql);
+    } 
+
     $sql = "SELECT * FROM `first_work`";
     $result = $conn->query($sql);
 
@@ -35,8 +45,10 @@ $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             array_push($students, $row);
-        } ?>
-    
+        } 
+    } 
+    ?>
+
     <? for ($i = 0; $i < sizeof($students); $i++) { ?>
     <form action method="post">
         <input type="text" name="id" value="<? echo $students[$i]["id"] ?>" hidden>
@@ -44,15 +56,6 @@ $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
         <input type="datetime-local" name="date" value="<? echo $students[$i]["action_date"] ?>" required>
         <input type="submit" value="Редактировать">
     </form>
-
-    <? }}
-    if (isset($_POST["fullNameEdit"]) || isset($_POST["date"]))
-        $id = $_POST["id"];
-        $fullName = trim($_POST["fullNameEdit"]);
-        $date = $_POST["date"];
-
-        $sql = "UPDATE `first_work` SET value = '$fullName', action_date = '$date' WHERE id = '$id'";
-        $conn->query($sql);
-    ?>
+    <? } ?>
 </body>
 </html>
