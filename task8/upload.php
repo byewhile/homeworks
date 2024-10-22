ce63a6da2c5926297ef6047c3d4b6c55703485a7
Aleksey <93428940+byewhile@users.noreply.github.com>
2024-10-22 18:49:02 +0300

    initial commit

---
+++ task8/upload.php
@@ -0,0 +1,36 @@
+<?php
+function uploadFile($fileName, $fileType, $fileTmp) {
+    $allowedFileTypes = ["image/jpeg", "image/gif"];
+
+    if (!in_array($fileType, $allowedFileTypes)) {
+        header("Location: index.php");
+        exit;
+    }
+
+	$converter = [
+		'а' => 'a',    'б' => 'b',    'в' => 'v',    'г' => 'g',    'д' => 'd',
+		'е' => 'e',    'ё' => 'e',    'ж' => 'zh',   'з' => 'z',    'и' => 'i',
+		'й' => 'y',    'к' => 'k',    'л' => 'l',    'м' => 'm',    'н' => 'n',
+		'о' => 'o',    'п' => 'p',    'р' => 'r',    'с' => 's',    'т' => 't',
+		'у' => 'u',    'ф' => 'f',    'х' => 'h',    'ц' => 'c',    'ч' => 'ch',
+		'ш' => 'sh',   'щ' => 'sch',  'ь' => '',     'ы' => 'y',    'ъ' => '',
+		'э' => 'e',    'ю' => 'yu',   'я' => 'ya',
+
+		'А' => 'A',    'Б' => 'B',    'В' => 'V',    'Г' => 'G',    'Д' => 'D',
+		'Е' => 'E',    'Ё' => 'E',    'Ж' => 'Zh',   'З' => 'Z',    'И' => 'I',
+		'Й' => 'Y',    'К' => 'K',    'Л' => 'L',    'М' => 'M',    'Н' => 'N',
+		'О' => 'O',    'П' => 'P',    'Р' => 'R',    'С' => 'S',    'Т' => 'T',
+		'У' => 'U',    'Ф' => 'F',    'Х' => 'H',    'Ц' => 'C',    'Ч' => 'Ch',
+		'Ш' => 'Sh',   'Щ' => 'Sch',  'Ь' => '',     'Ы' => 'Y',    'Ъ' => '',
+		'Э' => 'E',    'Ю' => 'Yu',   'Я' => 'Ya',
+    ];
+	$fileName = str_replace([' ', ','], '-', $fileName);
+	$fileName = strtr($fileName, $converter);
+
+    if (file_exists("files/$fileName")) {
+        $fileName = "a$fileName";
+    }
+    move_uploaded_file($fileTmp, "files/$fileName");
+    header("Location: index.php");
+}
+uploadFile($_FILES["profilePicture"]["name"], $_FILES["profilePicture"]["type"], $_FILES["profilePicture"]["tmp_name"]);
