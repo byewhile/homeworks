0affb640bc71dfb5b6a73b58cd49ce428ccbfcf5
Aleksey <93428940+byewhile@users.noreply.github.com>
2024-12-23 16:33:39 +0300

    Update index.php

--- task5/index.php
+++ task5/index.php
@@ -1,10 +1,10 @@
-<? require_once "components/head.php" ?>
+<?php require_once "components/head.php" ?>
     <title>Главная страница</title>
     <link rel="stylesheet" href="styles/home.css">
 </head>
 <body>
     <div class="wrapper">
-        <? require_once "components/header.php" ?>
+        <?php require_once "components/header.php" ?>
         <main>
             <div class="homeContainerText">
                 <p>Мы внедряем цифровые технологии</p>
